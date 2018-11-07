e2ec0c8c6d3b2247e481fd3e84e54e997a931766
Felix <f.meier@chal-tec.com>
2018-11-07 12:36:08 +0100

    tests in new project

---
+++ ESBOrder.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace ESBOrder.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Smoketest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task Request_With_Query()
+        {
+            //var query = new Dictionary<String, StringValues>();
+            //query.Add("name", "ushio");
+            //var body = "";
+
+            //var result = await HttpTrigger.RunAsync(req: HttpRequestSetup(query, body), log: log);
+            //var resultObject = (OkObjectResult)result;
+            //Assert.IsTrue(resultObject.Value != null && resultObject.Value.Contains("Hello, ushio"));
+            Assert.True(true);
+        }
+    }
+}
